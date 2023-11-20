ee7a5a8d865d24281848708f3bc6dab065aa1feb
tatarynrm <tatarynrm@gmail.com>
2023-11-20 12:41:46 +0200

    321132

--- src/pages/Cargo/Cargo.jsx
+++ src/pages/Cargo/Cargo.jsx
@@ -11,13 +11,16 @@ const Cargo = () => {
   const dispatch = useDispatch();
   const zap = useSelector((state) => state.zap.zap.items);
   const { status } = useSelector((state) => state.zap.zap);
-  const [krainaFilter,setKrainaFilter] = useState({})
+  const [krainaFilter,setKrainaFilter] = useState(null)
   useEffect(() => {
     dispatch(fetchAllZap());
   }, []);
 const uniqKraina =   uniqKrainaZorKrainaR(zap);
 // console.log(uniqKraina);
 console.log(krainaFilter);
+useEffect(()=>{
+
+},[krainaFilter])
   return (
     <>
       {zap.length <= 0 ? (
@@ -45,14 +48,17 @@ console.log(krainaFilter);
           flexDirection={"column"}
         >
           <Box display={'flex'} gap={'10px'} flexWrap={'wrap'}>
-            {uniqKraina && uniqKraina.map(item =>{
-              return <Button onClick={()=>{
+            {uniqKraina && uniqKraina.map((item,idx) =>{
+              return <Button key={idx} color={"teal"} onClick={()=>{
                 setKrainaFilter({
                   krainaZav:item.countryZav ,
                   krainaRozv: item.countryRozv
                 })
               }}>{item.countryZav} - {item.countryRozv} </Button>
             })}
+            <Button onClick={()=> {
+                  return  setKrainaFilter(null)
+            }}>Скинути фільтр</Button>
           </Box>
           <SimpleGrid
             spacing={5}
@@ -65,7 +71,7 @@ console.log(krainaFilter);
           >
             {zap
               .filter((item) => item.DATUPDATE)
-              .filter(item => krainaFilter.length > 0 ? item.ZAVKRAINA === krainaFilter.krainaZav && item.ROZVKRAINA === krainaFilter.krainaRozv : item)
+              .filter(item => krainaFilter !== null  ? item.ZAVKRAINA === krainaFilter.krainaZav & item.ROZVKRAINA === krainaFilter.krainaRozv : item)
               .sort(
                 (a, b) => toTimestamp(b?.DATUPDATE) - toTimestamp(a?.DATUPDATE)
               )
